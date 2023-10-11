43db15d008dc6975316389abcaea63eaf095d90f
hanisahmk <32774195+hanisahmk@users.noreply.github.com>
2023-10-11 09:29:54 +0800

    Update Pages.js

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,12 +1,22 @@
 import Home from "./Home";
-
-import React from "react";
+import Cuisine from "./Cuisine";
+import Searched from "../components/Searched";
+import Recipe from "./Recipe";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
+  const location = useLocation();
+
   return (
-    <div>
-      <Home />
-    </div>
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
